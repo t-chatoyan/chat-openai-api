99208079c10c8104d6785688c30ca8fea0771c77
Tiran Chatoyan <chatoyantiran5@gmail.com>
2023-05-26 20:17:21 +0400

    added answers for several questions

--- app/Http/Services/OpenAiService.php
+++ app/Http/Services/OpenAiService.php
@@ -4,20 +4,28 @@ namespace App\Http\Services;
 
 
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 class OpenAiService
 {
+    /**
+     * @param $question
+     * @return mixed
+     */
     public static function sendQuestion($question){
 
         $api_key = env('OPENAI_API_KEY');
         $api_endpoint = "https://api.openai.com/v1/chat/completions";
-
         $request_body = [
             "model" => "gpt-3.5-turbo",
-            "messages" => [["role" => "user", "content" => $question]],
+            "messages" => [
+                ["role" => "system", "content" => "My name is MSU"],
+                ["role" => "system", "content" => "I was created by Joe."],
+                ["role" => "user", "content" => $question],
+            ],
             "temperature" => 0,
-            "max_tokens" => 2048,
+            "max_tokens" => 3000,
         ];
 
         $response = Http::withHeaders([
